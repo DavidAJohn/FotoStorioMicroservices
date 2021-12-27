2900f7515d10159c1fb07bf0b6be03416fea8fc6
David John <davidajohn@gmail.com>
2021-12-27 22:38:13 +0000

    Updated the Program class to seed initial data

--- fotostorio-microservices/Services/Products/Products.API/Program.cs
+++ fotostorio-microservices/Services/Products/Products.API/Program.cs
@@ -1,19 +1,41 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Products.API.Data;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Products.API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public async static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<ApplicationDbContext>();
+                    await context.Database.MigrateAsync();
+
+                    await SeedData.SeedBrandsDataAsync(context);
+                    await SeedData.SeedCategoriesDataAsync(context);
+                    await SeedData.SeedMountsDataAsync(context);
+                    await SeedData.SeedProductsDataAsync(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding initial data");
+                }
+            }
+
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
