438b16ce1a58f47e38eb62b961eef31e70b612be
David John <davidajohn@gmail.com>
2021-12-30 14:38:07 +0000

    Created Product DTOs

---
+++ fotostorio-microservices/Services/Products/Products.API/DTOs/ProductCreateDTO.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Products.API.DTOs
+{
+    public class ProductCreateDTO
+    {
+        [Required]
+        public string Sku { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        public int BrandId { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public int MountId { get; set; }
+    }
+}
---
+++ fotostorio-microservices/Services/Products/Products.API/DTOs/ProductDTO.cs
@@ -0,0 +1,31 @@
+﻿namespace Products.API.DTOs
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+
+        public string Sku { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int BrandId { get; set; }
+
+        public string Brand { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string Category { get; set; }
+
+        public int MountId { get; set; }
+
+        public string Mount { get; set; }
+
+        public bool IsAvailable { get; set; }
+    }
+}
