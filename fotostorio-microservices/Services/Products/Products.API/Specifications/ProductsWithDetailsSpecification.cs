c956f59429222c10b0ef2322722c544da922db40
David John <davidajohn@gmail.com>
2021-12-31 13:40:20 +0000

    Updated product specs to only show available products

--- fotostorio-microservices/Services/Products/Products.API/Specifications/ProductsWithDetailsSpecification.cs
+++ fotostorio-microservices/Services/Products/Products.API/Specifications/ProductsWithDetailsSpecification.cs
@@ -10,7 +10,8 @@ namespace Products.API.Specifications
                 (string.IsNullOrEmpty(productParams.Search) || p.Name.ToLower().Contains(productParams.Search)) &&
                 (!productParams.BrandId.HasValue || p.BrandId == productParams.BrandId) &&
                 (!productParams.CategoryId.HasValue || p.CategoryId == productParams.CategoryId) &&
-                (!productParams.MountId.HasValue || p.MountId == productParams.MountId)
+                (!productParams.MountId.HasValue || p.MountId == productParams.MountId) &&
+                p.IsAvailable == true
             )
         {
             // Includes
