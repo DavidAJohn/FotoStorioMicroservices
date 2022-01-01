3e627c94be7276afd72732687c170887ec0bb176
David John <davidajohn@gmail.com>
2022-01-01 21:37:22 +0000

    Added a Basket repository

---
+++ fotostorio-microservices/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,45 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCache;
+
+        public BasketRepository(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache;
+        }
+
+        public async Task<CustomerBasket> GetBasketAsync(string basketId)
+        {
+            var basket = await _redisCache.GetStringAsync(basketId);
+
+            return string.IsNullOrEmpty(basket) ? null : JsonSerializer.Deserialize<CustomerBasket>(basket);
+        }
+
+        public async Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket)
+        {
+            var options = new DistributedCacheEntryOptions
+            {
+                SlidingExpiration = TimeSpan.FromDays(30)
+            };
+
+            var source = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+
+            await _redisCache.SetStringAsync(basket.Id, JsonSerializer.Serialize(basket), options, source.Token);
+
+            return await GetBasketAsync(basket.Id);
+        }
+
+        public async Task DeleteBasketAsync(string basketId)
+        {
+            await _redisCache.RemoveAsync(basketId);
+        }
+    }
+}
---
+++ fotostorio-microservices/Services/Basket/Basket.API/Repositories/IBasketRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Basket.API.Entities;
+
+namespace Basket.API.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<CustomerBasket> GetBasketAsync(string basketId);
+        Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket);
+        Task DeleteBasketAsync(string basketId);
+    }
+}
