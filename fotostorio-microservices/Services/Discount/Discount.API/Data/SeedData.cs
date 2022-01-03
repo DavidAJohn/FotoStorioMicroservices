f32742ab063ea0fc7ffe689fe9a90b139ce3f143
David John <davidajohn@gmail.com>
2022-01-03 16:52:23 +0000

    Set up Discount db seed data

---
+++ fotostorio-microservices/Services/Discount/Discount.API/Data/SeedData.cs
@@ -0,0 +1,52 @@
+﻿using Discount.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Discount.API.Data
+{
+    public static class SeedData
+    {
+        public static async Task SeedCampaignDataAsync(ApplicationDbContext context)
+        {
+            if (await context.Campaigns.AnyAsync()) return;
+
+            var campaignsData = await File.ReadAllTextAsync("./Data/SeedData/campaigns.json");
+            var campaigns = JsonSerializer.Deserialize<List<Campaign>>(campaignsData);
+
+            using (var transaction = context.Database.BeginTransaction())
+            {
+                foreach (var campaign in campaigns)
+                {
+                    campaign.StartDate = DateTime.Now;
+                    campaign.EndDate = DateTime.Now.AddDays(90);
+
+                    context.Campaigns.Add(campaign);
+                }
+
+                context.Database.ExecuteSqlInterpolated($"SET IDENTITY_INSERT Campaigns ON;");
+                context.SaveChanges();
+                context.Database.ExecuteSqlInterpolated($"SET IDENTITY_INSERT Campaigns OFF");
+                transaction.Commit();
+            }
+        }
+
+        public static async Task SeedProductDiscountDataAsync(ApplicationDbContext context)
+        {
+            if (await context.ProductDiscounts.AnyAsync()) return;
+
+            var discountsData = await File.ReadAllTextAsync("./Data/SeedData/discounts.json");
+            var discounts = JsonSerializer.Deserialize<List<ProductDiscount>>(discountsData);
+
+            foreach (var discount in discounts)
+            {
+                context.ProductDiscounts.Add(discount);
+            }
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
--- fotostorio-microservices/Services/Discount/Discount.API/Program.cs
+++ fotostorio-microservices/Services/Discount/Discount.API/Program.cs
@@ -1,22 +1,42 @@
+using Discount.API.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Discount.API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<ApplicationDbContext>();
+                    await context.Database.MigrateAsync();
+
+                    await SeedData.SeedCampaignDataAsync(context);
+                    await SeedData.SeedProductDiscountDataAsync(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding initial discount data");
+                }
+            }
+
+            await host.RunAsync();
         }
 
-        // Additional configuration is required to successfully run gRPC on macOS.
-        // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
