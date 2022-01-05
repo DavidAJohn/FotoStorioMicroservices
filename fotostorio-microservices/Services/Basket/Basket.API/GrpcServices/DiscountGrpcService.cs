a346c629ed6e27619f439521071dd08a5c50b24b
David John <davidajohn@gmail.com>
2022-01-05 19:18:29 +0000

    Created a client gRPC service class to consume the Discount API

---
+++ fotostorio-microservices/Services/Basket/Basket.API/GrpcServices/DiscountGrpcService.cs
@@ -0,0 +1,22 @@
+﻿using Discount.Api.Protos;
+using System.Threading.Tasks;
+
+namespace Basket.API.GrpcServices
+{
+    public class DiscountGrpcService
+    {
+        private readonly DiscountProtoService.DiscountProtoServiceClient _discountProtoService;
+
+        public DiscountGrpcService(DiscountProtoService.DiscountProtoServiceClient discountProtoService)
+        {
+            _discountProtoService = discountProtoService;
+        }
+
+        public async Task<DiscountModel> GetDiscount(string sku)
+        {
+            var discountRequest = new GetCurrentDiscountBySkuRequest { Sku = sku };
+
+            return await _discountProtoService.GetCurrentDiscountBySkuAsync(discountRequest);
+        }
+    }
+}
