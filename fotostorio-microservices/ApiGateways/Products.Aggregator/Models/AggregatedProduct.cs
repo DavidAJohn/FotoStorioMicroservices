7de7250df9da5c9e4dac65651969b16d1ad200c7
David John <davidajohn@gmail.com>
2022-01-07 12:19:54 +0000

    Created new Products.Aggregator microservice and added model classes

---
+++ fotostorio-microservices/ApiGateways/Products.Aggregator/Models/AggregatedProduct.cs
@@ -0,0 +1,33 @@
+﻿namespace Products.Aggregator.Models
+{
+    public class AggregatedProduct
+    {
+        public int Id { get; set; }
+
+        public string Sku { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public decimal SalePrice { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int BrandId { get; set; }
+
+        public string Brand { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string Category { get; set; }
+
+        public int MountId { get; set; }
+
+        public string Mount { get; set; }
+
+        public bool IsAvailable { get; set; }
+    }
+}
---
+++ fotostorio-microservices/ApiGateways/Products.Aggregator/Models/DiscountResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Products.Aggregator.Models
+{
+    public class DiscountResponse
+    {
+        public int Id { get; set; }
+        public string Sku { get; set; }
+        public decimal SalePrice { get; set; }
+    }
+}
