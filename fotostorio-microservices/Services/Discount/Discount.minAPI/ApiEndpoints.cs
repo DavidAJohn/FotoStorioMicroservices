60d1abad99f4f7902a8e9542a0717b586719450d
David John <davidajohn@gmail.com>
2022-01-07 11:19:42 +0000

    Configured the Discount.minAPI API endpoints

---
+++ fotostorio-microservices/Services/Discount/Discount.minAPI/ApiEndpoints.cs
@@ -0,0 +1,192 @@
+﻿namespace Discount.minAPI;
+
+public static class ApiEndpoints
+{
+    public static void ConfigureApiEndpoints(this WebApplication app)
+    {
+        // Discount endpoints
+        app.MapGet("api/Discounts/sku/{sku:maxlength(9)}", GetCurrentDiscountBySku);
+        app.MapGet("api/Discounts/id/{id:int}", GetCurrentDiscountById);
+        app.MapGet("api/Discounts/current", GetCurrentDiscounts);
+        app.MapGet("api/Discounts", GetAllDiscounts);
+        app.MapPost("api/Discounts", CreateDiscount);
+        app.MapPut("api/Discounts", UpdateDiscount);
+        app.MapDelete("api/Discounts/{id:int}", DeleteDiscount);
+
+        // Campaign endpoints
+        app.MapGet("api/Campaigns/id/{id:int}", GetCampaignById);
+        app.MapGet("api/Campaigns/current", GetCurrentCampaigns);
+        app.MapGet("api/Campaigns", GetAllCampaigns);
+        app.MapPost("api/Campaigns", CreateCampaign);
+        app.MapPut("api/Campaigns", UpdateCampaign);
+        app.MapDelete("api/Campaigns/{id:int}", DeleteCampaign);
+    }
+
+    private static async Task<IResult> GetAllDiscounts(IDiscountData data)
+    {
+        try
+        {
+            return Results.Ok(await data.GetAllDiscounts());
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    }
+
+    private static async Task<IResult> GetCurrentDiscounts(IDiscountData data)
+    {
+        try
+        {
+            return Results.Ok(await data.GetCurrentDiscounts());
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    }
+
+    private static async Task<IResult> GetCurrentDiscountBySku(string sku, IDiscountData data)
+    {
+        try
+        {
+            var results = await data.GetCurrentDiscountBySku(sku);
+            if (results == null) return Results.NotFound();
+            return Results.Ok(results);
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    }
+
+    private static async Task<IResult> GetCurrentDiscountById(int id, IDiscountData data)
+    {
+        try
+        {
+            var results = await data.GetCurrentDiscountById(id);
+            if (results == null) return Results.NotFound();
+            return Results.Ok(results);
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    }
+
+    private static async Task<IResult> CreateDiscount(ProductDiscount discount, IDiscountData data)
+    {
+        try
+        {
+            await data.CreateDiscount(discount);
+            return Results.Ok();
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    }
+
+    private static async Task<IResult> UpdateDiscount(ProductDiscount discount, IDiscountData data)
+    {
+        try
+        {
+            await data.UpdateDiscount(discount);
+            return Results.Ok();
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    }
+
+    private static async Task<IResult> DeleteDiscount(int id, IDiscountData data)
+    {
+        try
+        {
+            await data.DeleteDiscount(id);
+            return Results.Ok();
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    }
+
+    private static async Task<IResult> GetAllCampaigns(ICampaignData data)
+    {
+        try
+        {
+            return Results.Ok(await data.GetAllCampaigns());
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    }
+
+    private static async Task<IResult> GetCurrentCampaigns(ICampaignData data)
+    {
+        try
+        {
+            return Results.Ok(await data.GetCurrentCampaigns());
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    }
+
+    private static async Task<IResult> GetCampaignById(int id, ICampaignData data)
+    {
+        try
+        {
+            var results = await data.GetCampaignById(id);
+            if (results == null) return Results.NotFound();
+            return Results.Ok(results);
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    }
+
+    private static async Task<IResult> CreateCampaign(Campaign campaign, ICampaignData data)
+    {
+        try
+        {
+            await data.CreateCampaign(campaign);
+            return Results.Ok();
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    }
+
+    private static async Task<IResult> UpdateCampaign(Campaign campaign, ICampaignData data)
+    {
+        try
+        {
+            await data.UpdateCampaign(campaign);
+            return Results.Ok();
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    }
+
+    private static async Task<IResult> DeleteCampaign(int id, ICampaignData data)
+    {
+        try
+        {
+            await data.DeleteCampaign(id);
+            return Results.Ok();
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    }
+}
--- fotostorio-microservices/Services/Discount/Discount.minAPI/Program.cs
+++ fotostorio-microservices/Services/Discount/Discount.minAPI/Program.cs
@@ -1,41 +1,24 @@
+using Discount.minAPI;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddSingleton<ISqlDiscountAccess, SqlDiscountAccess>();
+builder.Services.AddSingleton<ISqlCampaignAccess, SqlCampaignAccess>();
+builder.Services.AddSingleton<IDiscountData, DiscountData>();
+builder.Services.AddSingleton<ICampaignData, CampaignData>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-       new WeatherForecast
-       (
-           DateTime.Now.AddDays(index),
-           Random.Shared.Next(-20, 55),
-           summaries[Random.Shared.Next(summaries.Length)]
-       ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+// API endpoint mapping:-> ./ApiEndpoints.cs
+app.ConfigureApiEndpoints();
 
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
