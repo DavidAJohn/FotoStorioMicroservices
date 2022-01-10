dbc1e08bdf788f2897b8569210d1a3ca7bb354cb
David John <davidajohn@gmail.com>
2022-01-10 18:15:12 +0000

    Added a Store Blazor Wasm project incorporating TailwindCSS v3

---
+++ fotostorio-microservices/ClientApps/Store.BlazorWasm/tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    content: [
+        "./Pages/*.razor",
+        "./Pages/**/*.razor",
+        "./Shared/*.razor",
+        "./App.razor"
+    ],
+  theme: {
+      container: {
+          center: true,
+          padding: '2rem'
+      },
+      extend: {
+          spacing: {
+              '112': '28rem',
+              '128': '32rem',
+              '144': '36rem',
+          },
+          borderRadius: {
+              '4xl': '2rem',
+          },
+          colors: {
+              fotoblue: '#111750',
+              fotofuchsia: '#FF2A57'
+          },
+          fontFamily: {
+              body: ['Roboto']
+          }
+      }
+  },
+  plugins: [],
+}
