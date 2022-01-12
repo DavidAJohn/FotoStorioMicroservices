a12bbe068275213e5db207ae0574977fb7adcd79
David John <davidajohn@gmail.com>
2022-01-12 12:46:25 +0000

    Big overhaul to the store layout, including navbar, footer and content

---
+++ fotostorio-microservices/ClientApps/Store.BlazorWasm/wwwroot/js/initialiseCarousel.js
@@ -0,0 +1,14 @@
+﻿function initialiseCarousel() {
+    const promoCarousel = document.querySelector('#promo-carousel');
+
+    const carousel = new bootstrap.Carousel(promoCarousel, {
+        interval: 5000 // interval between slides in ms
+    });
+
+    // this shifts focus to and from a separate page element 
+    // to help improve consistency of carousel initialisation,
+    // although the user clicking on the page should be sufficient 
+    const navElement = document.querySelector('#navbar-top');
+    navElement.focus();
+    navElement.blur();
+}
