ee22aea250ab27ea2b8c31cb905a2fd64352e17b
David John <davidajohn@gmail.com>
2022-01-13 20:08:22 +0000

    Added basic basket model classes to the store

---
+++ fotostorio-microservices/ClientApps/Store.BlazorWasm/Models/Basket.cs
@@ -0,0 +1,19 @@
+﻿namespace Store.BlazorWasm.Models;
+
+public class Basket
+{
+    public List<BasketItem> BasketItems { get; set; } = new();
+
+    public Decimal BasketTotal {
+        get {
+            decimal total = (decimal)0.0;
+
+            foreach (var item in BasketItems)
+            {
+                total += item.Total;
+            }
+
+            return total;
+        }
+    }
+}
---
+++ fotostorio-microservices/ClientApps/Store.BlazorWasm/Models/BasketItem.cs
@@ -0,0 +1,14 @@
+﻿namespace Store.BlazorWasm.Models;
+
+public class BasketItem
+{
+    public int Quantity { get; set; }
+    public Product Product { get; set; }
+
+    public decimal Total {
+        get {
+            var currentPrice = Product.SalePrice != 0 && Product.SalePrice < Product.Price ? Product.SalePrice : Product.Price;
+            return currentPrice * Quantity;
+        }
+    }
+}
