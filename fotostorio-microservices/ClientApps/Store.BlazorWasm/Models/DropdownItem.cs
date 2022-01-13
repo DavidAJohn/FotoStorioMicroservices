0b745b2cf6d30645fa402b5ed87a40f1c7fef2e4
David John <davidajohn@gmail.com>
2022-01-13 10:47:30 +0000

    Created new components for searching and sorting of products

---
+++ fotostorio-microservices/ClientApps/Store.BlazorWasm/Models/DropdownItem.cs
@@ -0,0 +1,8 @@
+﻿namespace Store.BlazorWasm.Models;
+
+public class DropdownItem
+{
+    public int Id { get; set; }
+    public string OptionName { get; set; }
+    public string OptionRef { get; set; }
+}
