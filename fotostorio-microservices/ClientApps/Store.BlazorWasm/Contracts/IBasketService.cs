47ba817d1b27433a5d697047f9725f573e20d2ab
David John <davidajohn@gmail.com>
2022-01-15 19:33:25 +0000

    Created a basket service for the store

---
+++ fotostorio-microservices/ClientApps/Store.BlazorWasm/Contracts/IBasketService.cs
@@ -0,0 +1,10 @@
+﻿using Store.BlazorWasm.Models;
+
+namespace Store.BlazorWasm.Contracts;
+
+public interface IBasketService
+{
+    Task<Basket> GetBasketByID(string id);
+    Task<Basket> UpdateBasket(Basket basket);
+    Task DeleteBasket(string id);
+}
---
+++ fotostorio-microservices/ClientApps/Store.BlazorWasm/Services/BasketService.cs
@@ -0,0 +1,74 @@
+﻿using Store.BlazorWasm.Contracts;
+using Store.BlazorWasm.Models;
+using System.Net.Http.Json;
+
+namespace Store.BlazorWasm.Services;
+
+public class BasketService : IBasketService
+{
+    private readonly IHttpClientFactory _httpClient;
+    private readonly ILogger<BasketService> _logger;
+
+    public BasketService(IHttpClientFactory httpClient, ILogger<BasketService> logger)
+    {
+        _httpClient = httpClient;
+        _logger = logger;
+    }
+
+    public async Task<Basket> GetBasketByID(string id)
+    {
+        try
+        {
+            var client = _httpClient.CreateClient("BasketAPI");
+            var basket = await client.GetFromJsonAsync<Basket>($"Basket?id={id}");
+
+            if (basket == null)
+            {
+                return new Basket{ };
+            }
+
+            return basket;
+        }
+        catch (HttpRequestException ex)
+        {
+            _logger.LogError($"Basket could not be retrieved: {id}");
+            throw new HttpRequestException(ex.Message);
+        }
+    }
+
+    public async Task<Basket> UpdateBasket(Basket basket)
+    {
+        try
+        {
+            var client = _httpClient.CreateClient("BasketAPI");
+            var response = await client.PostAsJsonAsync($"Basket", basket);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                _logger.LogError($"Basket could not be updated: {basket.Id}");
+                return null;
+            }
+
+            return await response.Content.ReadFromJsonAsync<Basket>();
+        }
+        catch (HttpRequestException ex)
+        {
+            _logger.LogError(ex.StatusCode + " " + ex.Message);
+            throw new HttpRequestException(ex.Message);
+        }
+    }
+
+    public async Task DeleteBasket(string id)
+    {
+        try
+        {
+            var client = _httpClient.CreateClient("BasketAPI");
+            await client.DeleteAsync($"Basket?id={id}");
+        }
+        catch (HttpRequestException ex)
+        {
+            _logger.LogError(ex.StatusCode + " " + ex.Message);
+            throw new HttpRequestException(ex.Message);
+        }
+    }
+}
