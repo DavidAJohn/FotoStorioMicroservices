30c59369694385bea7d5a44f1b4f970f6b3bcb46
David John <davidajohn@gmail.com>
2022-01-18 11:55:33 +0000

    Created non-functional login and register pages

---
+++ fotostorio-microservices/ClientApps/Store.BlazorWasm/Models/RegisterModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Store.BlazorWasm.Models;
+
+public class RegisterModel
+{
+    [Required]
+    [Display(Name = "Name")]
+    public string DisplayName { get; set; }
+
+    [Required]
+    [EmailAddress(ErrorMessage = "Email Address is invalid")]
+    [Display(Name = "Email")]
+    public string Email { get; set; }
+
+    [Required]
+    [StringLength(24, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 8)]
+    [DataType(DataType.Password)]
+    [Display(Name = "Password")]
+    public string Password { get; set; }
+
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirm password")]
+    [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+    public string ConfirmPassword { get; set; }
+}
