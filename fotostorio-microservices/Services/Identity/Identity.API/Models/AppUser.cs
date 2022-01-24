4e846924818a06a15ef1f601baa02bd54f1dd541
David John <davidajohn@gmail.com>
2022-01-24 16:18:52 +0000

    Added new Identity.API microservice and installed required packages

---
+++ fotostorio-microservices/Services/Identity/Identity.API/Models/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Identity.API.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string DisplayName { get; set; }
+        public Address Address { get; set; }
+    }
+}
