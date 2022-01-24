7a9c0894d45342a0aac0cc1ba169edc76c68d8f0
David John <davidajohn@gmail.com>
2022-01-24 18:23:08 +0000

    Added login and register models to identity project

---
+++ fotostorio-microservices/Services/Identity/Identity.API/Models/LoginModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity.API.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [EmailAddress(ErrorMessage = "Email Address is invalid")]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ fotostorio-microservices/Services/Identity/Identity.API/Models/LoginResult.cs
@@ -0,0 +1,9 @@
+﻿namespace Identity.API.Models
+{
+    public class LoginResult
+    {
+        public bool Successful { get; set; }
+        public string Error { get; set; }
+        public string Token { get; set; }
+    }
+}
