da645b9a41d9067392cdd9468394e213624de23b
David John <davidajohn@gmail.com>
2022-01-25 20:15:26 +0000

    Added new login display component to main layout

---
+++ fotostorio-microservices/ClientApps/Store.BlazorWasm/Extensions/ClaimsPrincipleExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System.Security.Claims;
+
+namespace Store.BlazorWasm.Extensions;
+
+public static class ClaimsPrincipalExtensions
+{
+    public static string GetDisplayNameFromPrincipal(this ClaimsPrincipal user)
+    {
+        string userName = "";
+
+        foreach (var claim in user.Claims)
+        {
+            if (claim.Type.ToString() == "given_name")
+            {
+                userName = claim.Value;
+            }
+        }
+
+        return userName;
+    }
+
+    public static string GetRoleFromPrincipal(this ClaimsPrincipal user)
+    {
+        string role = "";
+
+        foreach (var claim in user.Claims)
+        {
+            if (claim.Type.ToString() == "role")
+            {
+                role = claim.Value;
+            }
+        }
+
+        return role;
+    }
+    public static string GetEmailFromPrincipal(this ClaimsPrincipal user)
+    {
+        string email = "";
+
+        foreach (var claim in user.Claims)
+        {
+            if (claim.Type.ToString() == "email")
+            {
+                email = claim.Value;
+            }
+        }
+
+        return email;
+    }
+}
