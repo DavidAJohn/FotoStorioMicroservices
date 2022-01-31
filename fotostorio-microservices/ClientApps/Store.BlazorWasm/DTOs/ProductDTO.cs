95c40b76b0e6c0b91d12c48bc050ca4e31d90759
David John <davidajohn@gmail.com>
2022-01-31 17:24:45 +0000

    Simplified basket/product info saved in Redis and sent to Ordering.API

---
+++ fotostorio-microservices/ClientApps/Store.BlazorWasm/DTOs/ProductDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace Store.BlazorWasm.DTOs;
+
+public class ProductDTO
+{
+    public int Id { get; set; }
+    public string Sku { get; set; }
+    public string Name { get; set; }
+    public string ImageUrl { get; set; }
+    public decimal Price { get; set; }
+    public string Brand { get; set; }
+    public string Category { get; set; }
+    public string Mount { get; set; }
+}
--- fotostorio-microservices/ClientApps/Store.BlazorWasm/Models/BasketItem.cs
+++ fotostorio-microservices/ClientApps/Store.BlazorWasm/Models/BasketItem.cs
@@ -1,14 +1,15 @@
-﻿namespace Store.BlazorWasm.Models;
+﻿using Store.BlazorWasm.DTOs;
+
+namespace Store.BlazorWasm.Models;
 
 public class BasketItem
 {
     public int Quantity { get; set; }
-    public Product Product { get; set; }
+    public ProductDTO Product { get; set; }
 
     public decimal Total {
         get {
-            var currentPrice = Product.SalePrice != 0 && Product.SalePrice < Product.Price ? Product.SalePrice : Product.Price;
-            return currentPrice * Quantity;
+            return Product.Price * Quantity;
         }
     }
 }
