8daeff734ee2666db01ce4a90be0103c1dc8f84a
David John <davidajohn@gmail.com>
2022-01-31 20:23:05 +0000

    Added a basic customer orders page to the store

---
+++ fotostorio-microservices/ClientApps/Store.BlazorWasm/Extensions/DateTimeOffsetExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Store.BlazorWasm.Extensions;
+
+public static class DateTimeOffsetExtensions
+{
+    public static string ToUKStandardDate(this DateTimeOffset dateTime)
+    {
+        // returns date as dd/mm/yy (eg. 30/07/2021)
+        return String.Format("{0:dd/MM/yyyy}", dateTime);
+    }
+}
