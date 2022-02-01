8a9c4ddd07cc2e5a2c2978c3bdc00f2248099d49
David John <davidajohn@gmail.com>
2022-02-01 19:52:15 +0000

    Store checkout page now shows Stripe card input element

---
+++ fotostorio-microservices/ClientApps/Store.BlazorWasm/wwwroot/js/stripe.js
@@ -0,0 +1,41 @@
+﻿function setupStripe(publishable_key) {
+
+    let stripe = Stripe(publishable_key);
+
+    let elements = stripe.elements();
+
+    let style = {
+        base: {
+            iconColor: "#111750",
+            color: "#111750",
+            fontFamily: 'Roboto, Open Sans, Segoe UI, sans-serif',
+            fontSmoothing: "antialiased",
+            fontSize: "16px",
+            "::placeholder": {
+                color: "#111750"
+            }
+        },
+        invalid: {
+            iconColor: "#FF2A57",
+            color: "#FF2A57"
+        }
+    };
+
+    let card = elements.create('card', { style: style });
+
+    mountCard(card);
+
+    card.on('change', function (event) {
+        document.querySelector("#btnPlaceOrder").disabled = event.empty;
+        document.querySelector("#card-error").textContent = event.error ? event.error.message : "";
+    });
+};
+
+const mountCard = (card) => {
+    let checkExists = setInterval(function () {
+        if (document.querySelector("#card-element") !== null) {
+            card.mount("#card-element")
+            clearInterval(checkExists);
+        }
+    }, 100);
+};
