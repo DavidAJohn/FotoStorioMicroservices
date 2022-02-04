989c07998c20554c04cbba9dce7df711147bc6ea
David John <davidajohn@gmail.com>
2022-02-04 12:47:35 +0000

    Added an order details page to the store and corrected checkout redirect

--- fotostorio-microservices/ClientApps/Store.BlazorWasm/DTOs/OrderDetailsDTO.cs
+++ fotostorio-microservices/ClientApps/Store.BlazorWasm/DTOs/OrderDetailsDTO.cs
@@ -8,7 +8,7 @@ public class OrderDetailsDTO
     public string BuyerEmail { get; set; }
     public DateTimeOffset OrderDate { get; set; }
     public Address SendToAddress { get; set; }
-    public List<OrderItem> Items { get; set; }
+    public List<OrderItemReceivedDTO> Items { get; set; }
     public decimal Total { get; set; }
     public string Status { get; set; }
 }
---
+++ fotostorio-microservices/ClientApps/Store.BlazorWasm/DTOs/OrderItemReceivedDTO.cs
@@ -0,0 +1,19 @@
+﻿using Store.BlazorWasm.Models;
+
+namespace Store.BlazorWasm.DTOs;
+
+public class OrderItemReceivedDTO
+{
+    public OrderItemReceivedDTO(Product product, decimal price, int quantity)
+    {
+        Product = product;
+        Price = price;
+        Quantity = quantity;
+    }
+
+    public Product Product { get; set; }
+
+    public decimal Price { get; set; }
+
+    public int Quantity { get; set; }
+}
