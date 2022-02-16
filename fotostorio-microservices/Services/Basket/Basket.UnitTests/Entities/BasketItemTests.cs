c397966deaac58558138afb103b3c10acb7192b4
David John <davidajohn@gmail.com>
2022-02-16 12:40:14 +0000

    Added unit tests to the Basket microservice

---
+++ fotostorio-microservices/Services/Basket/Basket.UnitTests/Entities/BasketItemTests.cs
@@ -0,0 +1,64 @@
+﻿using Basket.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Basket.UnitTests.Entities
+{
+    public class BasketItemTests
+    {
+        [Fact]
+        public void Create_new_basket_item_success()
+        {
+            //Arrange
+            var product = new Product
+            {
+                Id = 1,
+                Sku = "TEST_SKU",
+                Name = "Product Name",
+                Price = 1,
+                ImageUrl = "image.jpg",
+                Brand = "Product Brand",
+                Category = "Product Category",
+                Mount = "Product Mount"
+            };
+
+            //Act
+            var basketItem = new BasketItem
+            {
+                Quantity = 1,
+                Product = product
+            };
+
+            //Assert
+            Assert.NotNull(basketItem);
+        }
+
+        [Fact]
+        public void Total_new_basket_item_correct()
+        {
+            //Arrange
+            var product = new Product
+            {
+                Id = 1,
+                Sku = "TEST_SKU",
+                Name = "Product Name",
+                Price = 10
+            };
+
+            //Act
+            var basketItem = new BasketItem
+            {
+                Quantity = 2,
+                Product = product
+            };
+
+            //Assert
+            Assert.NotNull(basketItem);
+            Assert.Equal(20, basketItem.Total);
+        }
+    }
+}
---
+++ fotostorio-microservices/Services/Basket/Basket.UnitTests/Entities/CustomerBasketTests.cs
@@ -0,0 +1,108 @@
+﻿using Basket.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Basket.UnitTests.Entities
+{
+    public class CustomerBasketTests
+    {
+        [Fact]
+        public void Create_new_customer_basket_success()
+        {
+            //Arrange
+            var product1 = new Product
+            {
+                Id = 1,
+                Sku = "TEST_SKU1",
+                Name = "Product Name",
+                Price = 1
+            };
+
+            var product2 = new Product
+            {
+                Id = 2,
+                Sku = "TEST_SKU2",
+                Name = "Product Name",
+                Price = 1
+            };
+
+            var basketItem1 = new BasketItem
+            {
+                Quantity = 1,
+                Product = product1
+            };
+
+            var basketItem2 = new BasketItem
+            {
+                Quantity = 1,
+                Product = product2
+            };
+
+            var basketItems = new List<BasketItem>();
+            basketItems.Add(basketItem1);
+            basketItems.Add(basketItem2);
+
+            //Act
+            var basket = new CustomerBasket
+            {
+                Id = "Basket1",
+                BasketItems = basketItems
+            };
+
+            //Assert
+            Assert.NotNull(basket);
+        }
+
+        [Fact]
+        public void Total_new_customer_basket_correct()
+        {
+            //Arrange
+            var product1 = new Product
+            {
+                Id = 1,
+                Sku = "TEST_SKU1",
+                Name = "Product Name",
+                Price = 5
+            };
+
+            var product2 = new Product
+            {
+                Id = 2,
+                Sku = "TEST_SKU2",
+                Name = "Product Name",
+                Price = 10
+            };
+
+            var basketItem1 = new BasketItem
+            {
+                Quantity = 1,
+                Product = product1
+            };
+
+            var basketItem2 = new BasketItem
+            {
+                Quantity = 2,
+                Product = product2
+            };
+
+            var basketItems = new List<BasketItem>();
+            basketItems.Add(basketItem1);
+            basketItems.Add(basketItem2);
+
+            //Act
+            var basket = new CustomerBasket
+            {
+                Id = "Basket1",
+                BasketItems = basketItems
+            };
+
+            //Assert
+            Assert.NotNull(basket);
+            Assert.Equal(25, basket.BasketTotal);
+        }
+    }
+}
