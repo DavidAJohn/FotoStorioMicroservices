5f2325740d50e62c496df9860c3e7a74f3478d80
David John <davidajohn@gmail.com>
2022-02-22 11:56:29 +0000

    Inventory.API now also publishes InventoryZero events to event bus

--- fotostorio-microservices/Services/Inventory/Inventory.API/Services/InventoryService.cs
+++ fotostorio-microservices/Services/Inventory/Inventory.API/Services/InventoryService.cs
@@ -1,5 +1,7 @@
-﻿using Inventory.API.Contracts;
+﻿using EventBus.Messages.Events;
+using Inventory.API.Contracts;
 using Inventory.API.Entities;
+using MassTransit;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 
@@ -10,12 +12,14 @@ namespace Inventory.API.Services
         private readonly ILogger<InventoryService> _logger;
         private readonly IUpdateRepository _updateRepository;
         private readonly IStockRepository _stockRepository;
+        private readonly IPublishEndpoint _publishEndpoint;
 
-        public InventoryService(ILogger<InventoryService> logger, IUpdateRepository updateRepository, IStockRepository stockRepository)
+        public InventoryService(ILogger<InventoryService> logger, IUpdateRepository updateRepository, IStockRepository stockRepository, IPublishEndpoint publishEndpoint)
         {
             _logger = logger;
             _updateRepository = updateRepository;
             _stockRepository = stockRepository;
+            _publishEndpoint = publishEndpoint;
         }
 
         public async Task CreateUpdateFromPaymentReceived(Update update)
@@ -34,15 +38,30 @@ namespace Inventory.API.Services
 
                     await _stockRepository.Update(skuToUpdate);
 
-                    // TODO: send InventoryZero event message to event bus if needed
-                    //if (skuToUpdate.CurrentStock == 0)
-                    //{
-                    //}
+                    // Send an InventoryZero event message to event bus if needed
+                    if (skuToUpdate.CurrentStock == 0)
+                    {
+                        await SendInventoryZeroEvent(skuToUpdate.Sku);
+                    }
 
                     _logger.LogInformation("Inventory updated after payment received -> Sku: {sku}, Previous Stock: {prev}, Updated Stock: {upd}",
                         skuToUpdate.Sku, originalStockCount, skuToUpdate.CurrentStock);
                 }
             }
+        }
+
+        private async Task SendInventoryZeroEvent(string sku)
+        {
+            // Send InventoryZeroEvent to the event bus (RabbitMq) using MassTransit
+
+            var eventMessage = new InventoryZeroEvent // from ./AsyncMessaging/EventBus.Messages class library
+            {
+                Sku = sku
+            };
+
+            await _publishEndpoint.Publish(eventMessage);
+
+            _logger.LogInformation("InventoryZeroEvent published to event bus for Sku: {sku}", sku);
         }
     }
 }
