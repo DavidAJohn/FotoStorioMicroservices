c76441a82008802ba63fb0944183d722d4c5316b
David John <davidajohn@gmail.com>
2022-02-27 23:50:40 +0000

    Added a simple health check config to Discount.minAPI

--- fotostorio-microservices/Services/Discount/Discount.minAPI/Program.cs
+++ fotostorio-microservices/Services/Discount/Discount.minAPI/Program.cs
@@ -1,6 +1,11 @@
 using Discount.minAPI;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
+
+ConfigurationManager configuration = builder.Configuration;
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -9,6 +14,13 @@ builder.Services.AddSingleton<ISqlDiscountAccess, SqlDiscountAccess>();
 builder.Services.AddSingleton<ISqlCampaignAccess, SqlCampaignAccess>();
 builder.Services.AddSingleton<IDiscountData, DiscountData>();
 builder.Services.AddSingleton<ICampaignData, CampaignData>();
+
+builder.Services.AddHealthChecks()
+    .AddCheck("self", () => HealthCheckResult.Healthy(), new string[] { "Discount.minAPI" })
+    .AddSqlServer(
+                configuration.GetConnectionString("DiscountConnectionString"),
+                name: "DiscountDB-check",
+                tags: new string[] { "DiscountDB" });
 
 var app = builder.Build();
 
@@ -20,5 +32,15 @@ if (app.Environment.IsDevelopment())
 
 // API endpoint mapping:-> ./ApiEndpoints.cs
 app.ConfigureApiEndpoints();
+
+app.MapHealthChecks("/hc", new HealthCheckOptions()
+{
+    Predicate = _ => true,
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+app.MapHealthChecks("/liveness", new HealthCheckOptions
+{
+    Predicate = r => r.Name.Contains("self")
+});
 
 app.Run();
