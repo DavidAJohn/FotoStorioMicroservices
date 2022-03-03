e68d79dc8b51bee30fd036b059e1a366b5297d04
David John <davidajohn@gmail.com>
2022-03-03 18:05:49 +0000

    Created the ApplicationStatus project to show healthcheck-ui data

---
+++ fotostorio-microservices/ClientApps/ApplicationStatus/Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddHealthChecksUI()
+    .AddInMemoryStorage();
+    // The following database providers are also available:
+    // -----
+    //.AddSqlServerStorage("server=localhost;initial catalog=healthchecksui;user id=sa;password=Password12!");
+    //.AddSqliteStorage("Data Source = healthchecks.db");
+    //.AddPostgreSqlStorage("Host=localhost;Username=postgres;Password=Password12!;Database=healthchecksui");
+    //.AddMySqlStorage("Host=localhost;User Id=root;Password=Password12!;Database=UI");
+
+var app = builder.Build();
+
+app.MapGet("/", () => "For health checks, go to: /hc-ui");
+
+app.MapHealthChecksUI(options =>
+{
+    options.UIPath = "/hc-ui";
+    options.AddCustomStylesheet("./Styles/custom.css");
+    options.AsideMenuOpened = false;
+    //options.ApiPath = "/api";
+});
+
+app.Run();
