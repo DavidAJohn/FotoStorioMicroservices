40eb617bc4059646739d5a5b148753683a86bb7e
David John <davidajohn@gmail.com>
2022-03-08 16:38:25 +0000

    Improved test coverage on order model classes

--- fotostorio-microservices/Services/Ordering/Ordering.UnitTests/Models/BasketTests.cs
+++ fotostorio-microservices/Services/Ordering/Ordering.UnitTests/Models/BasketTests.cs
@@ -58,6 +58,40 @@ namespace Ordering.UnitTests.Models
         }
 
         [Fact]
+        public void Create_new_basket_with_Id_success()
+        {
+            //Arrange
+            var product1 = new Product
+            {
+                Id = 1,
+                Sku = "TEST_SKU1",
+                Name = "Product Name",
+                Price = 1
+            };
+
+            var basketItem1 = new BasketItem
+            {
+                Quantity = 1,
+                Product = product1
+            };
+
+            var basketItems = new List<BasketItem>();
+            basketItems.Add(basketItem1);
+
+            string id = "Basket1";
+
+            //Act
+            var basket = new Basket(id)
+            {
+                Id = id,
+                BasketItems = basketItems
+            };
+
+            //Assert
+            Assert.Equal("Basket1", basket.Id);
+        }
+
+        [Fact]
         public void Total_new_basket_correct()
         {
             //Arrange
--- fotostorio-microservices/Services/Ordering/Ordering.UnitTests/Models/OrderCreateDTOTests.cs
+++ fotostorio-microservices/Services/Ordering/Ordering.UnitTests/Models/OrderCreateDTOTests.cs
@@ -11,7 +11,7 @@ namespace Ordering.UnitTests.Models
     public class OrderCreateDTOTests
     {
         [Fact]
-        public void Create_new_OrderCreateDTO_NotNull()
+        public void Create_new_OrderCreateDTO_WithParams_NotNull()
         {
             //Arrange
             var sendToAddress = new Address
@@ -60,7 +60,67 @@ namespace Ordering.UnitTests.Models
             orderItems.Add(orderItem2);
 
             //Act
-            var order = new OrderCreateDTO
+            var order = new OrderCreateDTO(orderItems, sendToAddress)
+            {
+                Items = orderItems,
+                SendToAddress = sendToAddress
+            };
+
+            //Assert
+            Assert.NotNull(order);
+        }
+
+        [Fact]
+        public void Create_new_OrderCreateDTO_NoParams_NotNull()
+        {
+            //Arrange
+            var sendToAddress = new Address
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                Street = "1 Street Name",
+                SecondLine = "",
+                City = "City",
+                County = "County",
+                PostCode = "PC1 9XL"
+            };
+
+            var product1 = new ProductItemOrdered
+            {
+                Id = 1,
+                Sku = "TEST_SKU1",
+                Name = "Product Name",
+                Price = 1,
+                ImageUrl = "image.jpg"
+            };
+
+            var product2 = new ProductItemOrdered
+            {
+                Id = 2,
+                Sku = "TEST_SKU2",
+                Name = "Product Name",
+                Price = 1,
+                ImageUrl = "image.jpg"
+            };
+
+            var orderItem1 = new OrderItem
+            {
+                Quantity = 1,
+                Product = product1
+            };
+
+            var orderItem2 = new OrderItem
+            {
+                Quantity = 1,
+                Product = product2
+            };
+
+            var orderItems = new List<OrderItem>();
+            orderItems.Add(orderItem1);
+            orderItems.Add(orderItem2);
+
+            //Act
+            var order = new OrderCreateDTO()
             {
                 Items = orderItems,
                 SendToAddress = sendToAddress
