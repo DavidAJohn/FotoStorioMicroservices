ab49106b36cbf6fe2c0d715bbd1b3b0b109fcf56
David John <davidajohn@gmail.com>
2023-01-03 23:26:51 +0000

    Added Serilog to ProductsAPI with basic startup config in Program class

--- fotostorio-microservices/Services/Products/Products.API/Program.cs
+++ fotostorio-microservices/Services/Products/Products.API/Program.cs
@@ -5,104 +5,124 @@ using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.OpenApi.Models;
+using Serilog;
+using Serilog.Events;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+        .WriteTo.Console()
+        .WriteTo.Seq("http://seq:5341") // http://<Seq container name>:<default Seq ingestion port>
+        .CreateLogger();
 
-ConfigurationManager configuration = builder.Configuration;
-
-builder.Services.AddDbContext<ApplicationDbContext>(options => {
-    options.UseSqlServer(configuration.GetConnectionString("ProductsConnectionString"),
-        sqlServerOptionsAction: sqlOptions =>
-        {
-            sqlOptions.EnableRetryOnFailure(
-            maxRetryCount: 5,
-            maxRetryDelay: TimeSpan.FromSeconds(15),
-            errorNumbersToAdd: null);
-        });
-});
-
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped<IBrandRepository, BrandRepository>();
-builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-builder.Services.AddScoped<IMountRepository, MountRepository>();
-builder.Services.AddScoped<IProductsService, ProductsService>();
-
-// Mass Transit and RabbitMQ config
-builder.Services.AddMassTransit(config =>
+try
 {
-    config.AddConsumer<InventoryZeroConsumer>();
-    config.AddConsumer<InventoryRestoredConsumer>();
+    Log.Information("Starting application");
 
-    config.UsingRabbitMq((ctx, cfg) =>
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog();
+
+    ConfigurationManager configuration = builder.Configuration;
+
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
     {
-        cfg.Host(configuration["EventBusSettings:HostAddress"]);
+        options.UseSqlServer(configuration.GetConnectionString("ProductsConnectionString"),
+            sqlServerOptionsAction: sqlOptions =>
+            {
+                sqlOptions.EnableRetryOnFailure(
+                maxRetryCount: 5,
+                maxRetryDelay: TimeSpan.FromSeconds(15),
+                errorNumbersToAdd: null);
+            });
+    });
 
-        cfg.ReceiveEndpoint(EventBusConstants.InventoryZeroQueue, c =>
-        {
-            c.ConfigureConsumer<InventoryZeroConsumer>(ctx);
-        });
+    builder.Services.AddScoped<IProductRepository, ProductRepository>();
+    builder.Services.AddScoped<IBrandRepository, BrandRepository>();
+    builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+    builder.Services.AddScoped<IMountRepository, MountRepository>();
+    builder.Services.AddScoped<IProductsService, ProductsService>();
 
-        cfg.ReceiveEndpoint(EventBusConstants.InventoryRestoredQueue, c =>
+    // Mass Transit and RabbitMQ config
+    builder.Services.AddMassTransit(config =>
+    {
+        config.AddConsumer<InventoryZeroConsumer>();
+        config.AddConsumer<InventoryRestoredConsumer>();
+
+        config.UsingRabbitMq((ctx, cfg) =>
         {
-            c.ConfigureConsumer<InventoryRestoredConsumer>(ctx);
+            cfg.Host(configuration["EventBusSettings:HostAddress"]);
+
+            cfg.ReceiveEndpoint(EventBusConstants.InventoryZeroQueue, c =>
+            {
+                c.ConfigureConsumer<InventoryZeroConsumer>(ctx);
+            });
+
+            cfg.ReceiveEndpoint(EventBusConstants.InventoryRestoredQueue, c =>
+            {
+                c.ConfigureConsumer<InventoryRestoredConsumer>(ctx);
+            });
         });
     });
-});
 
-builder.Services.AddScoped<InventoryZeroConsumer>();
-builder.Services.AddScoped<InventoryRestoredConsumer>();
+    builder.Services.AddScoped<InventoryZeroConsumer>();
+    builder.Services.AddScoped<InventoryRestoredConsumer>();
 
-builder.Services.AddHttpContextAccessor();
+    builder.Services.AddHttpContextAccessor();
 
-builder.Services.AddHealthChecks()
-                .AddCheck("self", () => HealthCheckResult.Healthy(), new string[] { "ProductsAPI" })
-                .AddCheck("ProductsDB-check", new SqlConnectionHealthCheck(
-                            configuration.GetConnectionString("ProductsConnectionString")),
-                            HealthStatus.Unhealthy, new string[] { "ProductsDB" });
+    builder.Services.AddHealthChecks()
+                    .AddCheck("self", () => HealthCheckResult.Healthy(), new string[] { "ProductsAPI" })
+                    .AddCheck("ProductsDB-check", new SqlConnectionHealthCheck(
+                                configuration.GetConnectionString("ProductsConnectionString")),
+                                HealthStatus.Unhealthy, new string[] { "ProductsDB" });
 
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Products.API", Version = "v1" });
-});
+    builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Products.API", Version = "v1" });
+    });
 
-builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
+    builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
 
-builder.Services.AddControllers();
+    builder.Services.AddControllers();
 
-builder.Logging.AddConsole()
-               .AddDebug()
-               .AddConfiguration(configuration.GetSection("Logging"));
+    var app = builder.Build();
 
-var app = builder.Build();
+    await SeedProductData(app);
 
-await SeedProductData(app);
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Products.API v1"));
+    }
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Products.API v1"));
+    app.UseSerilogRequestLogging();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.MapHealthChecks("/liveness", new HealthCheckOptions
+    {
+        Predicate = r => r.Name.Contains("self")
+    });
+
+    app.UseHealthChecks("/hc", new HealthCheckOptions()
+    {
+        Predicate = _ => true,
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
+
+    app.Run();
 }
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.MapHealthChecks("/liveness", new HealthCheckOptions
+catch (Exception ex)
 {
-    Predicate = r => r.Name.Contains("self")
-});
-
-app.UseHealthChecks("/hc", new HealthCheckOptions()
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
 {
-    Predicate = _ => true,
-    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-});
-
-app.Run();
-
+    Log.CloseAndFlush();
+}
 
 async Task SeedProductData(IHost app)
 {
@@ -122,8 +142,7 @@ async Task SeedProductData(IHost app)
         }
         catch (Exception ex)
         {
-            var logger = scope.ServiceProvider.GetService<ILogger<Program>>();
-            logger.LogError(ex, "An error occurred while seeding initial product data");
+            Log.Error(ex, "An error occurred while seeding initial product data");
         }
     }
 }
