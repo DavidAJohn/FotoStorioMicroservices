878f9230c085ab7a95e057e2a3d131eb1fe3e8f3
David John <davidajohn@gmail.com>
2023-08-16 13:04:21 +0100

    Added a unit test project for the Products service

---
+++ fotostorio-microservices/Services/Products/Products.UnitTests/GlobalUsings.cs
@@ -0,0 +1,9 @@
+global using Products.API.Contracts;
+global using Products.API.Controllers;
+global using Products.API.DTOs;
+global using Products.API.Helpers;
+global using Xunit;
+global using FluentAssertions;
+global using NSubstitute;
+global using NSubstitute.ReturnsExtensions;
+global using AutoFixture;
