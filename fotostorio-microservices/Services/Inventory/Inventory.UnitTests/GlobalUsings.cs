9e4d7ca5c12ea1faa704d498ab416d76cb95349f
David John <davidajohn@gmail.com>
2023-08-18 15:00:11 +0100

    Added a unit test project for the Inventory API

---
+++ fotostorio-microservices/Services/Inventory/Inventory.UnitTests/GlobalUsings.cs
@@ -0,0 +1,8 @@
+global using Inventory.API.Contracts;
+global using Inventory.API.Controllers;
+global using Inventory.API.Helpers;
+global using Xunit;
+global using FluentAssertions;
+global using NSubstitute;
+global using NSubstitute.ReturnsExtensions;
+global using AutoFixture;
