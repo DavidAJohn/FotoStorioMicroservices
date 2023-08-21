f6f54d72f56c5206aed7fc897ab0d990f01fa15e
David John <davidajohn@gmail.com>
2023-08-21 14:59:27 +0100

    Added unit tests for the Inventory API controllers

---
+++ fotostorio-microservices/Services/Inventory/Inventory.UnitTests/Controllers/StockControllerTests.cs
@@ -0,0 +1,304 @@
+﻿using Inventory.API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Inventory.UnitTests.Controllers;
+public class StockControllerTests : ControllerBase
+{
+    private readonly ILogger<StockController> _logger;
+    private readonly IStockRepository _stockRepository;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly IHttpContextService _httpContextService;
+    private readonly DefaultHttpContext _context;
+
+    public StockControllerTests()
+    {
+        _stockRepository = Substitute.For<IStockRepository>();
+        _logger = Substitute.For<ILogger<StockController>>();
+        _httpContextAccessor = Substitute.For<IHttpContextAccessor>();
+        _httpContextService = Substitute.For<IHttpContextService>();
+        _context = new DefaultHttpContext();
+        _httpContextAccessor.HttpContext = _context;
+    }
+
+    private void SetupHttpContextService(bool isAdmin = false)
+    {
+        _httpContextService
+            .GetJwtFromContext(_context)
+            .Returns("jwt");
+
+        if (isAdmin)
+        {
+            _httpContextService
+                .GetClaimValueByType(_context, "role")
+                .Returns("Administrator");
+        }
+        else
+        {
+            _httpContextService
+                .GetClaimValueByType(_context, "role")
+                .Returns("User");
+        }
+    }
+
+    [Fact]
+    public async Task GetStock_ShouldReturnListOfStock_WhenStockExists()
+    {
+        // Arrange
+        var stockList = new List<Stock>
+        {
+            new Stock
+            {
+                Sku = "ABC1234567890",
+                Name = "Test Product",
+                CurrentStock = 10,
+                LastUpdated = DateTime.Now
+            }
+        };
+
+        _stockRepository.ListAllAsync().Returns(stockList);
+
+        var sut = new StockController(_logger, _stockRepository, _httpContextAccessor, _httpContextService);
+
+        // Act
+        var result = (OkObjectResult)await sut.GetStock();
+
+        // Assert
+        result.StatusCode.Should().Be(200);
+        result.Value.Should().BeOfType<List<Stock>>();
+    }
+
+    [Fact]
+    public async Task GetStock_ShouldReturnNotFound_WhenStockDoesNotExist()
+    {
+        // Arrange
+        _stockRepository.ListAllAsync().ReturnsNull();
+
+        var sut = new StockController(_logger, _stockRepository, _httpContextAccessor, _httpContextService);
+
+        // Act
+        var result = (NotFoundResult)await sut.GetStock();
+
+        // Assert
+        result.StatusCode.Should().Be(404);
+    }
+
+    [Fact]
+    public async Task GetStockBySku_ShouldReturnStock_WhenStockForSkuExists()
+    {
+        // Arrange
+        var stockItem = new Stock
+        {
+            Sku = "ABC1234567890",
+            Name = "Test Product",
+            CurrentStock = 10,
+            LastUpdated = DateTime.Now
+        };
+
+        _stockRepository.GetBySkuAsync(Arg.Any<string>()).Returns(stockItem);
+
+        var sut = new StockController(_logger, _stockRepository, _httpContextAccessor, _httpContextService);
+
+        // Act
+        var result = (OkObjectResult)await sut.GetStockBySku(stockItem.Sku);
+
+        // Assert
+        result.StatusCode.Should().Be(200);
+        result.Value.Should().BeOfType<Stock>();
+        result.Value.Should().BeEquivalentTo(stockItem);
+    }
+
+    [Fact]
+    public async Task GetStockBySku_ShouldReturnBadRequest_WhenSuppliedSkuIsNull()
+    {
+        // Arrange
+        _stockRepository.GetBySkuAsync(Arg.Any<string>()).ReturnsNull();
+
+        var sut = new StockController(_logger, _stockRepository, _httpContextAccessor, _httpContextService);
+
+        // Act
+        var result = (BadRequestResult)await sut.GetStockBySku(null!);
+
+        // Assert
+        result.StatusCode.Should().Be(400);
+    }
+
+    [Fact]
+    public async Task GetStockBySku_ShouldReturnNotFound_WhenStockForSkuDoesNotExist()
+    {
+        // Arrange
+        var stockItem = new Stock
+        {
+            Sku = "ABC1234567890",
+            Name = "Test Product",
+            CurrentStock = 10,
+            LastUpdated = DateTime.Now
+        };
+
+        _stockRepository.GetBySkuAsync(Arg.Any<string>()).ReturnsNull();
+
+        var sut = new StockController(_logger, _stockRepository, _httpContextAccessor, _httpContextService);
+
+        // Act
+        var result = (NotFoundResult)await sut.GetStockBySku(stockItem.Sku);
+
+        // Assert
+        result.StatusCode.Should().Be(404);
+    }
+
+    [Fact]
+    public async Task GetStockAtOrBelowLevel_ShouldReturnExpectedListOfStock_WhenStockExistsAtOrBelowSuppliedLevel()
+    {
+        // Arrange
+        var stockList = new List<Stock>
+        {
+            new Stock
+            {
+                Sku = "ABC1234567890",
+                Name = "Test Product 1",
+                CurrentStock = 5,
+                LastUpdated = DateTime.Now
+            },
+            new Stock
+            {
+                Sku = "DEF1234567890",
+                Name = "Test Product 2",
+                CurrentStock = 10,
+                LastUpdated = DateTime.Now
+            },
+            new Stock
+            {
+                Sku = "GHJ1234567890",
+                Name = "Test Product 3",
+                CurrentStock = 3,
+                LastUpdated = DateTime.Now
+            }
+        };
+
+        int requestedStockLevel = 5; // should return a list containing 2 items
+
+        _stockRepository
+            .GetByStockLevelAtOrBelow(requestedStockLevel)
+            .Returns(stockList.Where(x => x.CurrentStock <= requestedStockLevel).ToList());
+
+        var sut = new StockController(_logger, _stockRepository, _httpContextAccessor, _httpContextService);
+
+        // Act
+        var result = (OkObjectResult)await sut.GetStockAtOrBelowLevel(requestedStockLevel);
+
+        // Assert
+        result.StatusCode.Should().Be(200);
+
+        var returnedList = (List<Stock>)result.Value!;
+        returnedList.Should().BeOfType<List<Stock>>();
+        returnedList.Should().HaveCount(2);
+    }
+
+    [Fact]
+    public async Task GetStockAtOrBelowLevel_ShouldReturnNotFound_WhenNoStockExistsAtOrBelowSuppliedLevel()
+    {
+        // Arrange
+        _stockRepository.GetByStockLevelAtOrBelow(Arg.Any<int>()).ReturnsNull();
+
+        var sut = new StockController(_logger, _stockRepository, _httpContextAccessor, _httpContextService);
+
+        // Act
+        var result = (NotFoundResult)await sut.GetStockAtOrBelowLevel(int.MinValue);
+
+        // Assert
+        result.StatusCode.Should().Be(404);
+    }
+
+    [Fact]
+    public async Task CreateNewStockEntry_ShouldReturnCreated_WhenStockEntryCreatedSuccessfully()
+    {
+        // Arrange
+        var stockItem = new Stock
+        {
+            Sku = "ABC1234567890",
+            Name = "Test Product",
+            CurrentStock = 10,
+            LastUpdated = DateTime.Now
+        };
+
+        _stockRepository.Create(Arg.Any<Stock>()).Returns(stockItem);
+
+        SetupHttpContextService(isAdmin: true);
+        var sut = new StockController(_logger, _stockRepository, _httpContextAccessor, _httpContextService);
+
+        // Act
+        var result = (OkObjectResult)await sut.CreateNewStockEntry(stockItem);
+
+        // Assert
+        result.StatusCode.Should().Be(200);
+        result.Value.Should().BeOfType<Stock>();
+        result.Value.Should().BeEquivalentTo(stockItem);
+    }
+
+    [Fact]
+    public async Task CreateNewStockEntry_ShouldReturnUnauthorized_WhenUserIsNotAdministrator()
+    {
+        // Arrange
+        var stockItem = new Stock
+        {
+            Sku = "ABC1234567890",
+            Name = "Test Product",
+            CurrentStock = 10,
+            LastUpdated = DateTime.Now
+        };
+
+        _stockRepository.Create(Arg.Any<Stock>()).Returns(stockItem);
+
+        SetupHttpContextService(isAdmin: false);
+        var sut = new StockController(_logger, _stockRepository, _httpContextAccessor, _httpContextService);
+
+        // Act
+        var result = (UnauthorizedResult)await sut.CreateNewStockEntry(stockItem);
+
+        // Assert
+        result.StatusCode.Should().Be(401);
+    }
+
+    [Fact]
+    public async Task CreateNewStockEntry_ShouldReturnBadRequest_WhenStockEntryWasNotCreated()
+    {
+        // Arrange
+        var stockItem = new Stock
+        {
+            Sku = "ABC1234567890",
+            Name = "Test Product",
+            CurrentStock = 10,
+            LastUpdated = DateTime.Now
+        };
+
+        _stockRepository.Create(Arg.Any<Stock>()).ReturnsNull();
+
+        SetupHttpContextService(isAdmin: true);
+        var sut = new StockController(_logger, _stockRepository, _httpContextAccessor, _httpContextService);
+
+        // Act
+        var result = (BadRequestObjectResult)await sut.CreateNewStockEntry(stockItem);
+
+        // Assert
+        result.StatusCode.Should().Be(400);
+    }
+
+    [Fact]
+    public async Task CreateNewStockEntry_ShouldReturnBadRequest_WhenStockEntryToCreateIsNull()
+    {
+        // Arrange
+        var stockItem = null as Stock;
+
+        _stockRepository.Create(Arg.Any<Stock>()).ReturnsNull();
+
+        SetupHttpContextService(isAdmin: true);
+        var sut = new StockController(_logger, _stockRepository, _httpContextAccessor, _httpContextService);
+
+        // Act
+        var result = (BadRequestResult)await sut.CreateNewStockEntry(stockItem);
+
+        // Assert
+        result.StatusCode.Should().Be(400);
+    }
+}
---
+++ fotostorio-microservices/Services/Inventory/Inventory.UnitTests/Controllers/UpdatesControllerTests.cs
@@ -0,0 +1,284 @@
+﻿using Inventory.API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+
+namespace Inventory.UnitTests.Controllers;
+public class UpdatesControllerTests : ControllerBase
+{
+    private readonly ILogger<UpdatesController> _logger;
+    private readonly IInventoryService _inventoryService;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly IHttpContextService _httpContextService;
+    private readonly DefaultHttpContext _context;
+
+    public UpdatesControllerTests()
+    {
+        _logger = Substitute.For<ILogger<UpdatesController>>();
+        _inventoryService = Substitute.For<IInventoryService>();
+        _httpContextAccessor = Substitute.For<IHttpContextAccessor>();
+        _httpContextService = Substitute.For<IHttpContextService>();
+        _context = new DefaultHttpContext();
+        _httpContextAccessor.HttpContext = _context;
+    }
+
+    private void SetupHttpContextService(bool isAdmin = false)
+    {
+        _httpContextService
+            .GetJwtFromContext(_context)
+            .Returns("jwt");
+
+        if (isAdmin)
+        {
+            _httpContextService
+                .GetClaimValueByType(_context, "role")
+                .Returns("Administrator");
+        }
+        else
+        {
+            _httpContextService
+                .GetClaimValueByType(_context, "role")
+                .Returns("User");
+        }
+    }
+
+    [Fact]
+    public async Task GetUpdates_ShouldReturnListOfUpdates_WhenUpdatesExistAndUserIsAdministrator()
+    {
+        // Arrange
+        var updatesList = new List<Update>
+        {
+            new Update
+            {
+                Id = 1,
+                Sku = "ABC1234567890",
+                UpdatedAt = DateTime.Now,
+                Added = 10,
+                Removed = 0
+            }
+        };
+
+        SetupHttpContextService(isAdmin: true);
+        var token = _httpContextService.GetJwtFromContext(_context);
+
+        _inventoryService.GetUpdates(token).Returns(updatesList);
+
+        // Act
+        var sut = new UpdatesController(_logger, _inventoryService, _httpContextAccessor, _httpContextService);
+        var result = (OkObjectResult)await sut.GetUpdates();
+
+        // Assert
+        result.StatusCode.Should().Be(200);
+        result.Value.Should().BeOfType<List<Update>>();
+        result.Value.Should().BeEquivalentTo(updatesList);
+    }
+
+    [Fact]
+    public async Task GetUpdates_ShouldReturnUnauthorized_WhenUserIsNotAdministrator()
+    {
+        // Arrange
+        SetupHttpContextService(isAdmin: false);
+
+        var sut = new UpdatesController(_logger, _inventoryService, _httpContextAccessor, _httpContextService);
+
+        // Act
+        var result = (UnauthorizedResult)await sut.GetUpdates();
+
+        // Assert
+        result.StatusCode.Should().Be(401);
+    }
+
+    [Fact]
+    public async Task GetUpdates_ShouldReturnNotFound_WhenNoUpdatesExist()
+    {
+        // Arrange
+        SetupHttpContextService(isAdmin: true);
+        var token = _httpContextService.GetJwtFromContext(_context);
+
+        _inventoryService.GetUpdates(token).ReturnsNull();
+
+        var sut = new UpdatesController(_logger, _inventoryService, _httpContextAccessor, _httpContextService);
+
+        // Act
+        var result = (NotFoundResult)await sut.GetUpdates();
+
+        // Assert
+        result.StatusCode.Should().Be(404);
+    }
+
+    [Fact]
+    public async Task GetUpdatesBySku_ShouldReturnListOfUpdates_WhenUpdatesExistAndUserIsAdministrator()
+    {
+        // Arrange
+        var updatesList = new List<Update>
+        {
+            new Update
+            {
+                Id = 1,
+                Sku = "ABC1234567890",
+                UpdatedAt = DateTime.Now,
+                Added = 10,
+                Removed = 0
+            }
+        };
+
+        SetupHttpContextService(isAdmin: true);
+        var token = _httpContextService.GetJwtFromContext(_context);
+
+        _inventoryService.GetUpdatesBySku(updatesList.First().Sku, token).Returns(updatesList);
+
+        // Act
+        var sut = new UpdatesController(_logger, _inventoryService, _httpContextAccessor, _httpContextService);
+        var result = (OkObjectResult)await sut.GetUpdatesBySku(updatesList.First().Sku);
+
+        // Assert
+        result.StatusCode.Should().Be(200);
+        result.Value.Should().BeOfType<List<Update>>();
+        result.Value.Should().BeEquivalentTo(updatesList);
+    }
+
+    [Fact]
+    public async Task GetUpdatesBySku_ShouldReturnUnauthorized_WhenUserIsNotAdministrator()
+    {
+        // Arrange
+        SetupHttpContextService(isAdmin: false);
+
+        var sut = new UpdatesController(_logger, _inventoryService, _httpContextAccessor, _httpContextService);
+
+        // Act
+        var result = (UnauthorizedResult)await sut.GetUpdatesBySku("ABC1234567890");
+
+        // Assert
+        result.StatusCode.Should().Be(401);
+    }
+
+    [Fact]
+    public async Task GetUpdatesBySku_ShouldReturnNotFound_WhenNoUpdatesExist()
+    {
+        // Arrange
+        SetupHttpContextService(isAdmin: true);
+        var token = _httpContextService.GetJwtFromContext(_context);
+
+        _inventoryService.GetUpdatesBySku(Arg.Any<string>(), token).ReturnsNull();
+
+        var sut = new UpdatesController(_logger, _inventoryService, _httpContextAccessor, _httpContextService);
+
+        // Act
+        var result = (NotFoundResult)await sut.GetUpdatesBySku("ABC1234567890");
+
+        // Assert
+        result.StatusCode.Should().Be(404);
+    }
+
+    [Fact]
+    public async Task GetUpdatesBySku_ShouldReturnBadRequest_WhenSuppliedSkuIsNull()
+    {
+        // Arrange
+        var sut = new UpdatesController(_logger, _inventoryService, _httpContextAccessor, _httpContextService);
+
+        // Act
+        var result = (BadRequestResult)await sut.GetUpdatesBySku(null!);
+
+        // Assert
+        result.StatusCode.Should().Be(400);
+    }
+
+    [Fact]
+    public async Task CreateStockUpdate_ShouldReturnCreatedUpdate_WhenUpdateIsValidAndUserIsAdmin()
+    {
+        // Arrange
+        var updateToCreate = new UpdateCreateDTO
+        {
+            Sku = "ABC1234567890",
+            Added = 10,
+            Removed = 0
+        };
+
+        var createdUpdate = new Update
+        {
+            Id = 1,
+            Sku = "ABC1234567890",
+            UpdatedAt = DateTime.Now,
+            Added = 10,
+            Removed = 0
+        };
+
+        SetupHttpContextService(isAdmin: true);
+        var token = _httpContextService.GetJwtFromContext(_context);
+
+        _inventoryService.CreateUpdateFromAdmin(Arg.Any<UpdateCreateDTO>(), token).Returns(createdUpdate);
+
+        // Act
+        var sut = new UpdatesController(_logger, _inventoryService, _httpContextAccessor, _httpContextService);
+        var result = (OkObjectResult)await sut.CreateStockUpdate(updateToCreate);
+
+        // Assert
+        result.StatusCode.Should().Be(200);
+        result.Value.Should().BeOfType<Update>();
+        result.Value.Should().BeEquivalentTo(createdUpdate);
+    }
+
+    [Fact]
+    public async Task CreateStockUpdate_ShouldReturnUnauthorized_WhenUserIsNotAdministrator()
+    {
+        // Arrange
+        var updateToCreate = new UpdateCreateDTO
+        {
+            Sku = "ABC1234567890",
+            Added = 10,
+            Removed = 0
+        };
+
+        SetupHttpContextService(isAdmin: false);
+
+        var sut = new UpdatesController(_logger, _inventoryService, _httpContextAccessor, _httpContextService);
+
+        // Act
+        var result = (UnauthorizedResult)await sut.CreateStockUpdate(updateToCreate);
+
+        // Assert
+        result.StatusCode.Should().Be(401);
+    }
+
+    [Fact]
+    public async Task CreateStockUpdate_ShouldReturnBadRequest_WhenUpdateIsNull()
+    {
+        // Arrange
+        var updateToCreate = null as UpdateCreateDTO;
+
+        SetupHttpContextService(isAdmin: true);
+
+        var sut = new UpdatesController(_logger, _inventoryService, _httpContextAccessor, _httpContextService);
+
+        // Act
+        var result = (BadRequestResult)await sut.CreateStockUpdate(updateToCreate);
+
+        // Assert
+        result.StatusCode.Should().Be(400);
+    }
+
+    [Fact]
+    public async Task CreateStockUpdate_ShouldReturnBadRequest_WhenUpdateWasNotCreated()
+    {
+        // Arrange
+        var updateToCreate = new UpdateCreateDTO
+        {
+            Sku = "ABC1234567890",
+            Added = 10,
+            Removed = 0
+        };
+
+        SetupHttpContextService(isAdmin: true);
+        var token = _httpContextService.GetJwtFromContext(_context);
+
+        _inventoryService.CreateUpdateFromAdmin(Arg.Any<UpdateCreateDTO>(), token).ReturnsNull();
+
+        // Act
+        var sut = new UpdatesController(_logger, _inventoryService, _httpContextAccessor, _httpContextService);
+        var result = (BadRequestObjectResult)await sut.CreateStockUpdate(updateToCreate);
+
+        // Assert
+        result.StatusCode.Should().Be(400);
+    }
+}
