e0cea8330aa54b04aebe48434141ef8e1628bc37
David John <davidajohn@gmail.com>
2023-10-11 19:43:53 +0100

    Created a simple shared helper class for Product unit tests

---
+++ fotostorio-microservices/Services/Products/Products.UnitTests/TestBase.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Products.API.Helpers;
+using Product = Products.API.Models.Product;
+
+namespace Products.UnitTests;
+
+public class TestBase
+{
+    protected static IMapper BuildMapper()
+    {
+        var config = new MapperConfiguration(options =>
+        {
+            options.AddProfile(new AutoMapperProfiles());
+        });
+
+        return config.CreateMapper();
+    }
+
+    protected static Product CreateProductFixture()
+    {
+        Fixture fixture = new Fixture();
+        var product = fixture.Create<Product>();
+        product.Id = 1;
+
+        return product;
+    }
+}
