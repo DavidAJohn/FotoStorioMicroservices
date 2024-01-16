19c86fc4bc215c2179b61944a8bf58cf9de11990
David John <davidajohn@gmail.com>
2024-01-16 16:02:59 +0000

    Improved Basket Controller unit tests

--- fotostorio-microservices/Services/Basket/Basket.UnitTests/Controllers/BasketControllerTests.cs
+++ fotostorio-microservices/Services/Basket/Basket.UnitTests/Controllers/BasketControllerTests.cs
@@ -3,38 +3,107 @@ using Basket.API.Controllers;
 using Basket.API.Entities;
 using Basket.API.Repositories;
 using Microsoft.AspNetCore.Mvc;
-using Moq;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
+using NSubstitute;
+using FluentAssertions;
+using NSubstitute.ReturnsExtensions;
+using Basket.API.GrpcServices;
+using Discount.Grpc.Protos;
 
 namespace Basket.UnitTests.Controllers;
 
 public class BasketControllerTests : ControllerBase
 {
-    private readonly Mock<IBasketRepository> _basketRepositoryMock;
-    private readonly Mock<IMapper> _mapperMock;
+    private readonly IBasketRepository _basketRepository = Substitute.For<IBasketRepository>();
+    private readonly IMapper _mapper = BuildMapper();
 
-    public BasketControllerTests()
-    {
-        _basketRepositoryMock = new Mock<IBasketRepository>();
-        _mapperMock = new Mock<IMapper>();
-    }
+    private readonly int _fakeProductPrice = 50;
+    private readonly int _fakeDiscountPrice = 40;
 
     [Fact]
-    public async Task GetBasketById_success()
+    public async Task GetBasketById_ShouldReturnCustomerBasket_WhenBasketIdExists()
     {
         //Arrange
         var fakeBasketId = "1";
         var fakeBasket = GetFakeCustomerBasket(fakeBasketId);
 
         //Act
-        var basketController = new BasketController(_basketRepositoryMock.Object, _mapperMock.Object, null);
-        var actionResult = await basketController.GetBasketById(fakeBasketId);
+        var basketController = new BasketController(_basketRepository, _mapper, null);
+        _basketRepository.GetBasketAsync(fakeBasketId).Returns(fakeBasket);
+        var result = (OkObjectResult)await basketController.GetBasketById(fakeBasketId);
 
         //Assert
-        Assert.IsType<ActionResult<CustomerBasket>>(actionResult);
+        result.Value.Should().BeOfType<CustomerBasket>();
+        result.Value.Should().BeEquivalentTo(fakeBasket);
     }
+
+    [Fact]
+    public async Task GetBasketById_ShouldReturnCustomerBasket_WhenBasketIdDoesNotExist()
+    {
+        //Arrange
+        var fakeBasketId = "1";
+
+        //Act
+        var basketController = new BasketController(_basketRepository, _mapper, null);
+        _basketRepository.GetBasketAsync(fakeBasketId).ReturnsNull();
+        var result = (OkObjectResult)await basketController.GetBasketById(fakeBasketId);
+
+        //Assert
+        result.Value.Should().BeOfType<CustomerBasket>();
+    }
+
+    [Fact]
+    public async Task UpdateBasket_ShouldReturnCustomerBasket_WhenBasketIdExistsAndDiscountPriceIsLower()
+    {
+        //Arrange
+        var fakeBasketId = "1";
+        var fakeBasket = GetFakeCustomerBasket(fakeBasketId);
+        var fakeBasketDTO = GetFakeCustomerBasketDTO(fakeBasketId);
+
+        //Act
+        var discountGrpcService = Substitute.For<IDiscountGrpcService>();
+        discountGrpcService.GetDiscount(Arg.Any<string>())
+            .Returns(new DiscountModel { Id = 1, Sku = "fakeSku", SalePrice = _fakeDiscountPrice });
+
+        var basketController = new BasketController(_basketRepository, _mapper, discountGrpcService);
+
+        _basketRepository.GetBasketAsync(Arg.Any<string>()).Returns(fakeBasket);
+        _basketRepository.UpdateBasketAsync(Arg.Any<CustomerBasket>()).Returns(fakeBasket);
+
+        var result = (OkObjectResult)await basketController.UpdateBasket(fakeBasketDTO);
+
+        //Assert
+        result.Value.Should().BeOfType<CustomerBasket>();
+        result.Value.Should().BeEquivalentTo(fakeBasket);
+    }
+
+    [Fact]
+    public async Task UpdateBasket_ShouldReturnCustomerBasket_WhenBasketIdExistsWithNoDiscountPrice()
+    {
+        //Arrange
+        var fakeBasketId = "1";
+        var fakeBasket = GetFakeCustomerBasket(fakeBasketId);
+        var fakeBasketDTO = GetFakeCustomerBasketDTO(fakeBasketId);
+
+        //Act
+        var discountGrpcService = Substitute.For<IDiscountGrpcService>();
+        discountGrpcService.GetDiscount(Arg.Any<string>())
+            .Returns(new DiscountModel { Id = 1, Sku = "fakeSku", SalePrice = _fakeProductPrice });
+
+        var basketController = new BasketController(_basketRepository, _mapper, discountGrpcService);
+
+        _basketRepository.GetBasketAsync(Arg.Any<string>()).Returns(fakeBasket);
+        _basketRepository.UpdateBasketAsync(Arg.Any<CustomerBasket>()).Returns(fakeBasket);
+
+        var result = (OkObjectResult)await basketController.UpdateBasket(fakeBasketDTO);
+
+        //Assert
+        result.Value.Should().BeOfType<CustomerBasket>();
+        result.Value.Should().BeEquivalentTo(fakeBasket);
+    }
+
 
     private CustomerBasket GetFakeCustomerBasket(string basketId)
     {
@@ -65,11 +134,34 @@ public class BasketControllerTests : ControllerBase
             Id = id,
             Sku = "fakeSku",
             Name = "fakeName",
-            Price = 50,
+            Price = _fakeProductPrice,
             ImageUrl = "fakeUrl",
             Brand = "fakeBrand",
             Category = "fakeCategory",
             Mount = "fakeMount"
         };
+    }
+
+    private CustomerBasketDTO GetFakeCustomerBasketDTO(string basketId)
+    {
+        var basketItems = new List<BasketItem>();
+        basketItems.Add(GetFakeBasketItem(1));
+        basketItems.Add(GetFakeBasketItem(2));
+
+        return new CustomerBasketDTO()
+        {
+            Id = basketId,
+            BasketItems = basketItems
+        };
+    }
+
+    private static IMapper BuildMapper()
+    {
+        var config = new MapperConfiguration(options =>
+        {
+            options.AddProfile(new AutoMapperProfiles());
+        });
+
+        return config.CreateMapper();
     }
 }
