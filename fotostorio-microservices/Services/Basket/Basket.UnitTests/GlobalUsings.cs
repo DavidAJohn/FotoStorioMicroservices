cbd51604ac621f8ac58fb9dd34b171590c433054
David John <davidajohn@gmail.com>
2024-01-18 12:14:42 +0000

    Moved typical unit test packages to global usings file in Basket test project

--- fotostorio-microservices/Services/Basket/Basket.UnitTests/GlobalUsings.cs
+++ fotostorio-microservices/Services/Basket/Basket.UnitTests/GlobalUsings.cs
@@ -3,3 +3,6 @@ global using Basket.API.GrpcServices;
 global using Basket.API.Helpers;
 global using Basket.API.Repositories;
 global using Basket.API.Entities;
+global using NSubstitute;
+global using FluentAssertions;
+global using AutoFixture;
