fcca89cf97fcc6748d4a9a2adba2d250a05d2772
David John <davidajohn@gmail.com>
2024-06-27 14:58:24 +0100

    Added fotostorio-specific details to tailwind config file

--- fotostorio-microservices/ClientApps/Marketing.Ng/tailwind.config.js
+++ fotostorio-microservices/ClientApps/Marketing.Ng/tailwind.config.js
@@ -4,7 +4,27 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '2rem'
+    },
+    extend: {
+      spacing: {
+        '112': '28rem',
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+          '4xl': '2rem',
+      },
+      colors: {
+          fotoblue: '#111750',
+          fotofuchsia: '#FF2A57'
+      },
+      fontFamily: {
+          body: ['Roboto']
+      }
+    },
   },
   plugins: [],
 }
