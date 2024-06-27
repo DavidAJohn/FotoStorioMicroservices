2a5ed0c268da42baa7af2a87d90fd41d06c6eada
David John <davidajohn@gmail.com>
2024-06-27 15:17:22 +0100

    Created a basic fotostorio-style page layout

--- fotostorio-microservices/ClientApps/Marketing.Ng/src/app/app.component.ts
+++ fotostorio-microservices/ClientApps/Marketing.Ng/src/app/app.component.ts
@@ -10,4 +10,5 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'FotoStorio Marketing';
+  currentYear = new Date().getFullYear();
 }
--- fotostorio-microservices/ClientApps/Marketing.Ng/src/app/app.routes.ts
+++ fotostorio-microservices/ClientApps/Marketing.Ng/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
-
+    { path: 'home', component: HomeComponent },
+    
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
 ];
