4ec2d264941817196a53aa6ea809273fc5a4156a
David John <davidajohn@gmail.com>
2024-06-28 13:00:52 +0100

    Moved the footer to a separate component

--- fotostorio-microservices/ClientApps/Marketing.Ng/src/app/app.component.ts
+++ fotostorio-microservices/ClientApps/Marketing.Ng/src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { FooterComponent } from "./shared/footer/footer.component";
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
     standalone: true,
-    imports: [RouterOutlet]
+    imports: [RouterOutlet, FooterComponent]
 })
 export class AppComponent {
   title = 'FotoStorio Marketing';
-  currentYear = new Date().getFullYear();
 }
