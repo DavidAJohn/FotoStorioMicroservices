1e96f843fd56f25f3f51cbba8c26d2a522b1be3e
David John <davidajohn@gmail.com>
2024-06-28 13:37:43 +0100

    Added some simple paths to tsconfig

--- fotostorio-microservices/ClientApps/Marketing.Ng/src/app/app.component.ts
+++ fotostorio-microservices/ClientApps/Marketing.Ng/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { FooterComponent } from "./shared/footer/footer.component";
+import { FooterComponent } from "@shared/footer/footer.component";
 
 @Component({
     selector: 'app-root',
