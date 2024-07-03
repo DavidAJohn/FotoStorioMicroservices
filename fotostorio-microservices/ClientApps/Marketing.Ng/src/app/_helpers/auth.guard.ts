ba08b198393ec9be47c0621471c21695e520d14e
David John <davidajohn@gmail.com>
2024-07-03 13:54:24 +0100

    Created an auth guard to redirect unauthorised users

---
+++ fotostorio-microservices/ClientApps/Marketing.Ng/src/app/_helpers/auth.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+import { AuthenticationService } from '@app/_services';
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard {
+    constructor(
+        private router: Router,
+        private authenticationService: AuthenticationService
+    ) { }
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        const user = this.authenticationService.userValue;
+        if (user) {
+            // authorised so return true
+            return true;
+        }
+
+        // not logged in so redirect to login page with the return url
+        this.router.navigate(['/account/login'], { queryParams: { returnUrl: state.url } });
+        return false;
+    }
+}
--- fotostorio-microservices/ClientApps/Marketing.Ng/src/app/app.routes.ts
+++ fotostorio-microservices/ClientApps/Marketing.Ng/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './account/login.component';
+import { AuthGuard } from './_helpers/auth.guard';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent },
+    { path: 'home', component: HomeComponent, canActivate: [AuthGuard]  },
     { path: 'account/login', component: LoginComponent },
     
     // otherwise redirect to home
