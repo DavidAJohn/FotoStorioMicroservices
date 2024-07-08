fa8c24cc0f4f6d76c5d7ffb68f0c65eb715570f1
David John <davidajohn@gmail.com>
2024-07-08 15:27:34 +0100

    Updated routes to include individual order page

--- fotostorio-microservices/ClientApps/Marketing.Ng/src/app/app.routes.ts
+++ fotostorio-microservices/ClientApps/Marketing.Ng/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './account/login.component';
 import { AuthGuard } from './_helpers/auth.guard';
 import { UnauthorisedComponent } from './account/unauthorised.component';
 import { OrderListComponent } from './orders/order-list/order-list.component';
+import { OrderComponent } from './orders/order/order.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -11,6 +12,7 @@ export const routes: Routes = [
     { path: 'account/login', component: LoginComponent },
     { path: 'unauthorised', component: UnauthorisedComponent },
     { path: 'orders', component: OrderListComponent, canActivate: [AuthGuard]},
+    { path: 'order/:id', component: OrderComponent, canActivate: [AuthGuard]},
     
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
