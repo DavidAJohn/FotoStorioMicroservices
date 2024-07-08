0ff0cb462eb578427ea211d8c7de8a71ad539ded
David John <davidajohn@gmail.com>
2024-07-08 14:43:19 +0100

    Added order service method to get an individual order by id

--- fotostorio-microservices/ClientApps/Marketing.Ng/src/app/_services/order.service.ts
+++ fotostorio-microservices/ClientApps/Marketing.Ng/src/app/_services/order.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { environment } from '@environments/environment';
+import { Order } from '@app/_models';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +15,9 @@ export class OrderService {
     let days = environment.latestOrdersThreshold ?? 60;
 
     return this.http.get<any>(`${environment.marketingGatewayUrl}/marketing/orders/latest/${days}`)
+  }
+
+  getOrderById(id: number) {
+    return this.http.get<Order>(`${environment.marketingGatewayUrl}/marketing/orders/${id}`)
   }
 }
