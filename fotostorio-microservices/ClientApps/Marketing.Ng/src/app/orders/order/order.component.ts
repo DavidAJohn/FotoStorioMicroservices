4ca5640bce6100b547bd99380c115397941c608e
David John <davidajohn@gmail.com>
2024-07-09 14:27:22 +0100

    Order component now shows if an item was sold at a discount

--- fotostorio-microservices/ClientApps/Marketing.Ng/src/app/orders/order/order.component.ts
+++ fotostorio-microservices/ClientApps/Marketing.Ng/src/app/orders/order/order.component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterLink, ActivatedRoute } from '@angular/router';
 
 import { Order, OrderItem } from '@app/_models';
-import { OrderService } from '@app/_services';
+import { OrderService, DiscountService } from '@app/_services';
 
 @Component({
   selector: 'app-order',
@@ -16,8 +16,9 @@ export class OrderComponent {
   @Input() order?: Order;
   orderItems: OrderItem[] = [];
   errorMessage = '';
+  itemDiscounts: string[] = [];
 
-  constructor(private orderService: OrderService, private route: ActivatedRoute) {
+  constructor(private orderService: OrderService, private discountService: DiscountService, private route: ActivatedRoute) {
   }
 
   ngOnInit(): void {
@@ -30,6 +31,21 @@ export class OrderComponent {
       .subscribe(order => {
         this.order = order;
         this.orderItems = order.items;
+        
+        // Get discounts for each item
+        this.orderItems.forEach(item => {
+          this.getDiscountsForSkuByDate(item.product.sku, order.orderDate);
+        });
+      });
+  }
+
+  getDiscountsForSkuByDate(sku: string, orderDate: Date): void {
+    this.discountService.getDiscountsForSkuByDate(sku, orderDate)
+      .subscribe(discounts => {
+        // Add discounts to itemDiscounts array
+        discounts.forEach(discount => {
+          this.itemDiscounts.push(discount.sku);
+        });
       });
   }
 }
