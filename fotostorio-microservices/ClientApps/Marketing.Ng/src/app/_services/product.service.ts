d57466e5ce7bbcf0485bc89796f4351062257bb5
David John <davidajohn@gmail.com>
2024-07-11 17:53:31 +0100

    Added a method to the product service to get product details by sku

--- fotostorio-microservices/ClientApps/Marketing.Ng/src/app/_services/product.service.ts
+++ fotostorio-microservices/ClientApps/Marketing.Ng/src/app/_services/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { environment } from '@environments/environment';
+import { Product } from '@app/_models';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,10 @@ export class ProductService {
   constructor(private http: HttpClient) { }
 
   getProductCount() {
-    return this.http.get<any>(`${environment.marketingGatewayUrl}/marketing/catalog`, {observe: 'response'})
+    return this.http.get<Product[]>(`${environment.marketingGatewayUrl}/marketing/catalog`, {observe: 'response'})
+  }
+
+  getProductBySku(sku: string) {
+    return this.http.get<Product>(`${environment.marketingGatewayUrl}/marketing/catalog/sku/${sku}`)
   }
 }
