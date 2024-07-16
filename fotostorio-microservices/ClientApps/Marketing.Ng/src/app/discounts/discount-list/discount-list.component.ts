d4de0fa7878b799d6114a89b667f35ee8240bad8
David John <davidajohn@gmail.com>
2024-07-16 13:21:24 +0100

    Discount list now has a dropdown to allow selection by current, future and all discounts

--- fotostorio-microservices/ClientApps/Marketing.Ng/src/app/discounts/discount-list/discount-list.component.ts
+++ fotostorio-microservices/ClientApps/Marketing.Ng/src/app/discounts/discount-list/discount-list.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
-import { Discount, DiscountedProduct } from '@app/_models';
+import { Discount, DiscountedProduct, Product } from '@app/_models';
 import { DiscountService, ProductService } from '@app/_services';
 
 @Component({
@@ -17,7 +17,44 @@ export class DiscountListComponent {
   discounts: Discount[] = [];
   discountedProducts: DiscountedProduct[] = [];
 
+  optionsVisible = false;
+  optionsText = 'Current Discounts';
+
   constructor(private discountService: DiscountService, private productService: ProductService) {
+    this.getCurrentDiscounts();
+  }
+
+  toggleDropdown() {
+    this.optionsVisible = !this.optionsVisible;
+  }
+
+  handleOptionClick(option: number) {
+    this.discounts = [];
+    this.discountedProducts = [];
+
+    switch (option) {
+      case 0:
+        this.getCurrentDiscounts();
+        this.optionsText = 'Current Discounts';
+        break;
+      case 1:
+        this.getCurrentAndFutureDiscounts();
+        this.optionsText = 'Current & Future Discounts';
+        break;
+      case 2:
+        this.getAllDiscounts();
+        this.optionsText = 'All Discounts';
+        break;
+      default:
+        this.getCurrentDiscounts();
+        this.optionsText = 'Current Discounts';
+        break;
+    }
+
+    this.optionsVisible = false;
+  }
+
+  getCurrentDiscounts() {
     this.discountService.getCurrentDiscounts()
       .subscribe({
         next: discounts => {
@@ -28,18 +65,7 @@ export class DiscountListComponent {
             this.productService.getProductBySku(discount.sku)
               .subscribe({
                 next: product => {
-                  let discountedProduct = new DiscountedProduct();
-                  discountedProduct.productId = product.id;
-                  discountedProduct.discountId = discount.id;
-                  discountedProduct.sku = product.sku;
-                  discountedProduct.name = product.name;
-                  discountedProduct.price = product.price;
-                  discountedProduct.imageUrl = product.imageUrl;
-                  discountedProduct.category = product.category;
-                  discountedProduct.campaignId = discount.campaignId;
-                  discountedProduct.campaign = discount.campaign;
-                  discountedProduct.salePrice = product.salePrice;
-
+                  let discountedProduct = this.createDiscountedProduct(product, discount);
                   this.discountedProducts.push(discountedProduct);
                 },
                 error: error => {
@@ -54,5 +80,76 @@ export class DiscountListComponent {
           this.errorMessage = 'Error receiving discounts: ' + error;
         }
       });
+  }
+
+  getCurrentAndFutureDiscounts() {
+    this.discountService.getCurrentAndFutureDiscounts()
+      .subscribe({
+        next: discounts => {
+          this.discounts = discounts;
+
+          // Get details of the products that are discounted
+          this.discounts.forEach(discount => {
+            this.productService.getProductBySku(discount.sku)
+              .subscribe({
+                next: product => {
+                  let discountedProduct = this.createDiscountedProduct(product, discount);
+                  this.discountedProducts.push(discountedProduct);
+                },
+                error: error => {
+                  console.error('Error receiving discounted product: ', error);
+                  this.errorMessage = 'Error receiving discounted product: ' + error;
+                }
+              });
+          });
+        },
+        error: error => {
+          console.error('Error receiving discounts: ', error);
+          this.errorMessage = 'Error receiving discounts: ' + error;
+        }
+      });
+  }
+
+  getAllDiscounts() {
+    this.discountService.getAllDiscounts()
+      .subscribe({
+        next: discounts => {
+          this.discounts = discounts;
+
+          // Get details of the products that are discounted
+          this.discounts.forEach(discount => {
+            this.productService.getProductBySku(discount.sku)
+              .subscribe({
+                next: product => {
+                  let discountedProduct = this.createDiscountedProduct(product, discount);
+                  this.discountedProducts.push(discountedProduct);
+                },
+                error: error => {
+                  console.error('Error receiving discounted product: ', error);
+                  this.errorMessage = 'Error receiving discounted product: ' + error;
+                }
+              });
+          });
+        },
+        error: error => {
+          console.error('Error receiving discounts: ', error);
+          this.errorMessage = 'Error receiving discounts: ' + error;
+        }
+      });
+  }
+
+  private createDiscountedProduct(product: Product, discount: Discount): DiscountedProduct {
+    let discountedProduct = new DiscountedProduct();
+
+    discountedProduct.productId = product.id;
+    discountedProduct.sku = product.sku;
+    discountedProduct.name = product.name;
+    discountedProduct.price = product.price;
+    discountedProduct.campaign = discount.campaign;
+    discountedProduct.imageUrl = product.imageUrl;
+    discountedProduct.category = product.category;
+    discountedProduct.salePrice = discount.salePrice;
+
+    return discountedProduct;
   }
 }
