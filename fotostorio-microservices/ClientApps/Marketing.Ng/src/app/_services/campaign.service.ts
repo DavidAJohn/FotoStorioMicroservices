d73ba52e9c469fa24ac7412abc88ecf76c71ecbe
David John <davidajohn@gmail.com>
2024-07-17 11:18:58 +0100

    Added a post method to the campaign service to create new campaigns

--- fotostorio-microservices/ClientApps/Marketing.Ng/src/app/_services/campaign.service.ts
+++ fotostorio-microservices/ClientApps/Marketing.Ng/src/app/_services/campaign.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { environment } from '@environments/environment';
 import { Campaign } from '@app/_models';
@@ -21,5 +21,16 @@ export class CampaignService {
 
   getCampaignById(id: number) {
     return this.http.get<Campaign>(`${environment.marketingGatewayUrl}/marketing/campaigns/${id}`)
+  }
+
+  createCampaign(campaign: Campaign) {
+    let jsonCampaign = JSON.stringify(campaign);
+
+    const httpOptions = {
+      headers: new HttpHeaders({'Content-Type': 'application/json'}),
+      observe: 'response' as 'response'
+    }
+
+    return this.http.post(`${environment.marketingGatewayUrl}/marketing/campaigns`, jsonCampaign, httpOptions);
   }
 }
