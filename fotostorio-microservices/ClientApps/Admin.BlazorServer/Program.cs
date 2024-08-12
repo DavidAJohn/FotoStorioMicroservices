e066f040033f0a48cdc8714bc848f355a7c6eb5f
David John <davidajohn@gmail.com>
2024-08-12 13:21:17 +0100

    Updated the Admin project to use new Resilience Handlers instead

--- fotostorio-microservices/ClientApps/Admin.BlazorServer/Program.cs
+++ fotostorio-microservices/ClientApps/Admin.BlazorServer/Program.cs
@@ -4,33 +4,26 @@ using Admin.BlazorServer.Services;
 using Blazored.LocalStorage;
 using Blazored.Modal;
 using Blazored.Toast;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.Http.Resilience;
 using Polly;
-using Polly.Extensions.Http;
-using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
 builder.Services.AddHttpClient("AdminGateway", c => c.BaseAddress =
-    new Uri(builder.Configuration["ApiSettings:AdminGatewayUri"]))
-    .AddPolicyHandler(GetRetryPolicy())
-    .AddPolicyHandler(GetCircuitBreakerPolicy());
+    new Uri(builder.Configuration["ApiSettings:AdminGatewayUri"]!))
+    .AddStandardResilienceHandler(options => GetStandardResilienceOptions());
 
 builder.Services.AddHttpClient("IdentityAPI", c => c.BaseAddress =
-    new Uri(builder.Configuration["ApiSettings:IdentityUri"] + "/api/"))
-    .AddPolicyHandler(GetRetryPolicy())
-    .AddPolicyHandler(GetCircuitBreakerPolicy());
+    new Uri(builder.Configuration["ApiSettings:IdentityUri"]! + "/api/"))
+    .AddStandardResilienceHandler(options => GetStandardResilienceOptions());
 
 builder.Services.AddHttpClient("ApplicationStatus", c => c.BaseAddress =
-    new Uri(builder.Configuration["ApiSettings:AppStatusUri"]))
-    .AddPolicyHandler(GetRetryPolicy())
-    .AddPolicyHandler(GetCircuitBreakerPolicy());
+    new Uri(builder.Configuration["ApiSettings:AppStatusUri"]!))
+    .AddStandardResilienceHandler(options => GetStandardResilienceOptions());
 
 //builder.Services.AddApiAuthorization(opt => opt.UserOptions.RoleClaim = "role");
 builder.Services.AddAuthorizationCore(config =>
@@ -57,11 +50,9 @@ builder.Services.AddBlazoredModal();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -76,30 +67,17 @@ app.MapFallbackToPage("/_Host");
 
 app.Run();
 
-IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
-{
-    // Retry with jitter: https://github.com/App-vNext/Polly/wiki/Retry-with-jitter
-    Random jitter = new Random();
 
-    return HttpPolicyExtensions
-        .HandleTransientHttpError()
-        .WaitAndRetryAsync(
-            retryCount: 5,
-            sleepDurationProvider: retryAttempt =>
-                    TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))  // exponential backoff (2, 4, 8, 16, 32 secs)
-                  + TimeSpan.FromMilliseconds(jitter.Next(0, 1000)), // plus some jitter: up to 1 second
-            onRetry: (exception, retryCount, context) =>
-            {
-                Log.Warning($"Retry {retryCount} of {context.PolicyKey} at {context.OperationKey}, due to: {exception}.");
-            });
-}
-
-IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPolicy()
+static HttpStandardResilienceOptions GetStandardResilienceOptions()
 {
-    return HttpPolicyExtensions
-        .HandleTransientHttpError()
-        .CircuitBreakerAsync(
-            handledEventsAllowedBeforeBreaking: 5,
-            durationOfBreak: TimeSpan.FromSeconds(30)
-        );
+    var options = new HttpStandardResilienceOptions();
+
+    options.Retry.MaxRetryAttempts = 5;
+    options.Retry.UseJitter = true;
+    options.Retry.Delay = TimeSpan.FromSeconds(2);
+    options.Retry.BackoffType = DelayBackoffType.Exponential;
+
+    options.CircuitBreaker.BreakDuration = TimeSpan.FromSeconds(15);
+
+    return options;
 }
